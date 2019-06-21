800a1a8d46ffc1f6dcc4bd650e66b5796d290c6a
PopSylvestr <kozhin.a.v.n@gmail.com>
2019-06-21 19:12:11 -0400

    set results_as_hash

--- app1.rb
+++ app1.rb
@@ -2,19 +2,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
-
-configure do
-	db = get_db
-	db.execute 'CREATE TABLE IF NOT EXISTS "users" 
-	(
-	"id" INTEGER PRIMARY KEY AUTOINCREMENT, 
-	"username" TEXT, 
-	"phone" TEXT,
-	"datestamp" TEXT
-	"barber" TEXT
-	"color" TEXT);'
-end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -47,27 +34,13 @@ hh = {
 			@error hh.select {|key,_ params[key] == }.values.join(",")
 
 		if @error != ''
-					 erb visit
+					erb visit
 		end
-db = get_db
-			db.execute 'insert into Users (
-			username, 
-			phone, 
-			datestamp, 
-			barber, 
-			color
-			)	
-			values (?,?,?,?,?)', [@username, @phone, @datetime, @barber, @color]
+
+				
+
 		
 	
 		erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
-	end
-	get '/showusers' do 
-		erb "Hello world"
-	end
-	def get_db
-		db = SQLite3::Databbase.new 'barbershop.db'
-		db.results_as_hash = true
-		return db
 	end
